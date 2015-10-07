9dc009e91a16be5d27fa1f96f002ac30daa421b6
dhagmann <hagmann@cmu.edu>
2015-10-07 17:34:45 -0400

    Switched to doParallel library
    doParallel runs on Windows, Mac, and Linux.
    
    Function now has an option for parallel processing (true by default),
    and to set number of cores (all by default).

--- R/ibl_simulation.R
+++ R/ibl_simulation.R
@@ -4,11 +4,36 @@ ibl_simulation <- function(gambles,
                            noise = 0.25,
                            decay = 0.5,
                            foregone = F,
-                           cores = 1,
+                           parallel = T,
+                           cores = NULL,
                            iterations = 1000) {
-    library(parallel)
-    
     noptions <- length(gambles)
-    simulation_results <- mclapply(1:iterations, mc.cores=cores, FUN=function(x) ibl_new(noptions,prevalues,ntrials,noise,decay,gambles,foregone))
-    simulation_results <- do.call(cbind,simulation_results)
+    
+    if(parallel) {
+        library(doParallel) # Move this into recommended DEPENDENCIES once we create a package
+        
+        # Set the number of cores to be used
+        if(is.null(cores)) {
+            cl <- makeCluster(detectCores()/2,  # There may be a better way to do this?
+                              methods = F)      # If something breaks, remove this line. 
+                                                # Code runs faster with it.
+        } else {
+            cl <- makeCluster(cores,
+                              methods = F)      # If something breaks, remove this line.
+        }
+        
+        registerDoParallel(cl)
+        simulation_results <- foreach(i=1:iterations,
+                                      .combine = cbind,
+                                      .export = "ibl_new") %dopar%  # .export may not be needed if function is in package
+            ibl_new(noptions,prevalues,ntrials,noise,decay,gambles,foregone)
+        stopCluster(cl)
+    } else {
+        simulation_results <- foreach(i=1:iterations,
+                                      .combine = cbind,
+                                      .export = "ibl_new") %do%  # .export may not be needed if function is in package
+            ibl_new(noptions,prevalues,ntrials,noise,decay,gambles,foregone)
+    }
+    
+    return(simulation_results)
 }
