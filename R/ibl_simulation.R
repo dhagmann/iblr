0802dd39f20b1f5c9299dcad8c8eb412837599f0
dhagmann <hagmann@cmu.edu>
2015-10-07 19:14:11 -0400

    R Package setup
    Used devtools to create the folder structure for the library.

--- R/ibl_simulation.R
+++ R/ibl_simulation.R
@@ -1,3 +1,18 @@
+#' Simulates the IBL Model
+#' @param gambles A list of gambles containing payoffs in column 1 and associated probabilities in column 2
+#' @param ntrials The number of trials in the experiment
+#' @param prevalues The number of simulated draws before the start of the experiment
+#' @param noise The noise parameter of IBL
+#' @param decay The decoy parameter of IBL
+#' @param foregone Logical. Whether foregone outcomes are observed by the decision maker
+#' @param parallel Logical. Should the model run using parallel processing?
+#' @param cores Number of cores. If left blank, uses half of all available cores (fastest)
+#' @param iterations The number of iterations for the simulation
+#' 
+#' @return A matrix with the number of rows equal to \code{ntrials} and the number of columns equal to \code{iterations}.
+#' 
+#' @examples Simple example needed. (After fixing how gambles are handled?)
+
 ibl_simulation <- function(gambles,
                            ntrials,
                            prevalues,
@@ -7,6 +22,7 @@ ibl_simulation <- function(gambles,
                            parallel = T,
                            cores = NULL,
                            iterations = 1000) {
+    
     noptions <- length(gambles)
     
     if(parallel) {
